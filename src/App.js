4aff251515e9078e99e5f35ab030708cf8c67aff
skarma-iitb <24M0857@iitb.ac.in>
2024-10-15 16:37:29 +0530

    login+upload

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import LoginSignup from "./components/login";
 import Home from "./components/Home";
-<<<<<<< HEAD
 import Dashboard from "./components/Dashboard";
 import VideoPlayer from "./components/VideoPlayer";
-=======
->>>>>>> 413df8f (1st commit)
-
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
+      <Navbar />        
+        <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<LoginSignup />}></Route>
-<<<<<<< HEAD
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/video/:id" element={<VideoPlayer />} />
-=======
->>>>>>> 413df8f (1st commit)
-      </Routes>
+        </Routes>
+    
     </div>
   );
 };
