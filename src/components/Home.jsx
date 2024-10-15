cad91a7091a46fdbe5d24ec7929f5a68bd602223
skarma-iitb <24M0857@iitb.ac.in>
2024-10-15 15:02:10 +0530

    Add all the UI files to the repository

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoGrid from "./VideoGrid";
+
+const Home = () => {
+  return (
+    <div className="pt-24 px-8">
+      <h1 className="text-3xl font-bold mb-6">Home - Video Streaming App</h1>
+      <VideoGrid />
+    </div>
+  );
+};
+
+export default Home;
