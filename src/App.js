b47319b2e8cd0769766ce188cedb290fc7244bf0
skarma-iitb <24M0857@iitb.ac.in>
2024-11-12 15:27:59 +0530

    upload conflict resolved

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import LoginSignup from "./components/login";
 import Home from "./components/Home";
-<<<<<<< HEAD
 import Dashboard from "./components/Dashboard";
 import VideoPlayer from "./components/VideoPlayer";
-=======
-<<<<<<< HEAD
-//git
-=======
-import Dashboard from "./components/Dashboard";
-import VideoPlayer from "./components/VideoPlayer";
-
->>>>>>> 6f94e0f (Video-upload option added)
->>>>>>> 6fa05e7 (Video-upload option added on UI)
 const App = () => {
   return (
     <div className="App">
@@ -24,12 +14,7 @@ const App = () => {
         <Route path="/login" element={<LoginSignup />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/video/:id" element={<VideoPlayer />} />
-<<<<<<< HEAD
         </Routes>
-    
-=======
-      </Routes>
->>>>>>> 6fa05e7 (Video-upload option added on UI)
     </div>
   );
 };
